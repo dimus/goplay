ce3a4fef91d249a1eb2aeafb9a1f91ce36f5573f
Dmitry Mozzherin <dmozzherin@gmail.com>
2020-08-09 10:58:12 -0500

    natsm

--- natsm/go.mod
+++ natsm/go.mod
@@ -3,9 +3,8 @@ module github.com/dimus/goplay/natsm
 go 1.14
 
 require (
-	github.com/golang/protobuf v1.4.1
+	github.com/golang/protobuf v1.4.2
 	github.com/nats-io/nats-server/v2 v2.1.7 // indirect
 	github.com/nats-io/nats.go v1.10.0
-	google.golang.org/grpc v1.31.0
 	google.golang.org/protobuf v1.25.0
 )
--- natsm/main.go
+++ natsm/main.go
@@ -2,10 +2,7 @@ package main
 
 import (
 	"log"
-	"time"
 
-	nats "github.com/nats-io/nats.go"
-	"google.golang.org/grpc"
 	"google.golang.org/protobuf/proto"
 )
 
@@ -14,21 +11,13 @@ const (
 )
 
 func main() {
-
-	// Create NATS server connection
-	natsConnection, _ := nats.Connect(NatsURL)
-	log.Println("Connected to " + NatsURL)
-
-	msg, err := natsConnection.Request("Discovery.OrderService", nil, 1000*time.Millisecond)
-	if err == nil && msg != nil {
-		orderServiceDiscovery := ServiceDiscovery{}
-		err := proto.Unmarshal(msg.Data, &orderServiceDiscovery)
-		if err != nil {
-			log.Fatalf("Error on unmarshal: %v", err)
-		}
-		address := orderServiceDiscovery.OrderServiceUri
-		log.Println("OrderService endpoint found at:", address)
-		//Set up a connection to the gRPC server.
-		conn, err := grpc.Dial(address, grpc.WithInsecure())
+	uri := NatsURL
+	sd := &ServiceDiscovery{
+		OrderServiceUri: uri,
 	}
+	psd, err := proto.Marshal(sd)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_ = psd
 }
